1b0bfdddefffa60b2be7a5a2c75cd467d99ec159
CesarION02 <32071789+CesarION02@users.noreply.github.com>
2020-11-24 14:10:36 -0600

    Add files via upload

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+admin:$1$TE8QZWMK$wGdCom0D9F.aJ9ZTQQLmk/:Cesar Ivan Orozco Natividad:cesar.orozco@swaplicado.com.mx:admin,user
+cesarion:$1$X8KPguss$La5p3y99I0o2Q5eeCuCmD.:Cesar Ivan Orozco:cesar.i.orozco.n@gmail.com:user
